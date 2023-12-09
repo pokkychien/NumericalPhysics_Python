7373c8350f136faf214b70ed082a35ef95037f47
pokky <pokky.chien@gmail.com>
2023-12-09 13:51:27 +0800

    modified:   1d_box.py

--- 1d_box.py
+++ 1d_box.py
@@ -4,7 +4,7 @@
     Solve the particle in 1-dimentional box problem, and plot the wave function.
     Using numerical method for eigenvalue problem for Schrodinger equation.
 """
-
+modified by pokky
 import numpy as np
 
 def schrodinger (x):
